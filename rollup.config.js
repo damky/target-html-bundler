97a232e990626b69eac8f28dcfa5581a8b5c7255
Doug McKay <douglasamckay@gmail.com>
2021-05-01 16:13:35 -0600

    adding eslint prettier babel and rollup

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'iife'
+  },
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ]
+}
