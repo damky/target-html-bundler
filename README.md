050df40b50bad3e39f755461118dcca61404a819
Doug McKay <18455931+damky@users.noreply.github.com>
2021-05-02 13:03:17 -0600

    update readme and set up initial message

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # target-html-bundler
 bundles modern react workflow to es5 compatible iife functions wrapped in html that you can add to adobe target html offers.
+
+If you're running VSCode, hit f5 to start the debugger in watch mode, or do 'npm start'
--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react'
 function App () {
-  return <h1>Hello World From React-Rollup!</h1>
+  return <div>
+    <h1>You're live!</h1>
+    <h4>Add the following to a Resource Override js file:</h4>
+    <code>
+      {`(()=>{ const s = document.createElement('script'); s.src = 'http://localhost:3000/dist/bundle.js'; document.head.appendChild(s) })();`}
+    </code>
+    <p>then replace all this in src/App.js and change your entry selector in index.js</p>
+  </div>
 }
 export default App
