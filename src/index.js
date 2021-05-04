9204e66694165785de22a24ad94a6023a76daffa
Doug McKay <18455931+damky@users.noreply.github.com>
2021-05-04 10:59:40 -0600

    append app entry if not found

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-ReactDOM.render(<App />, document.querySelector('body'))
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const appEntryId = 'app-entry-id';
+const appEntry = document.getElementById(appEntryId);
+
+if (!appEntry) {
+  const el = document.createElement('div');
+  el.id = appEntryId;
+  document.querySelector('body').appendChild(el);
+}
+
+ReactDOM.render(<App />, document.getElementById(appEntryId));
