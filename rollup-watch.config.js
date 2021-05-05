6a5beb7ba0a23056d486ce615fedc06e7f8395a0
Doug McKay <18455931+damky@users.noreply.github.com>
2021-05-05 16:47:57 -0600

    set up preact

--- rollup-watch.config.js
+++ rollup-watch.config.js
@@ -3,6 +3,8 @@ import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
+import livereload from 'rollup-plugin-livereload'
+import serve from 'rollup-plugin-serve'
 import { terser } from 'rollup-plugin-terser'
 
 export default {
@@ -30,6 +32,14 @@ export default {
       presets: ['@babel/preset-react'],
       babelHelpers: 'inline'
     }),
-    commonjs()
+    commonjs(),
+    serve({
+      open: true,
+      verbose: true,
+      contentBase: ['', 'public'],
+      host: 'localhost',
+      port: 3000
+    }),
+    livereload({ watch: 'dist' })
   ]
 }
--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App () {
       {`(()=>{ const s = document.createElement('script'); s.src = 'http://localhost:3000/dist/bundle.js'; document.head.appendChild(s) })();`}
     </code>
     <p>then replace all this in src/App.js and change your entry selector in index.js</p>
+    <h4>Add to Target</h4>
+    <p>Once you're done developing, do 'npm run build' and copy the contents of dist/bundle.js to paste in a script tag</p>
   </div>
 }
 export default App
