1cb09c357a490f848cd38d525483cbd8ca3f555e
Doug McKay <18455931+damky@users.noreply.github.com>
2021-05-06 09:06:40 -0600

    adjusting boiler code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
 function App () {
-  return <div>
-    <h1>You're live!</h1>
-    <h4>Add the following to a Resource Override js file:</h4>
-    <code>
-      {`(()=>{ const s = document.createElement('script'); s.src = 'http://localhost:3000/dist/bundle.js'; document.head.appendChild(s) })();`}
-    </code>
-    <p>then replace all this in src/App.js and change your entry selector in index.js</p>
-    <h4>Add to Target</h4>
-    <p>Once you're done developing, do 'npm run build' and copy the contents of dist/bundle.js to paste in a script tag</p>
-  </div>
+  return (
+    <div>
+      <h1>You're live!</h1>
+      <h4>Add the following to a Resource Override js file:</h4>
+      <code>
+        {`(()=>{ const s = document.createElement('script'); s.src = 'http://localhost:3000/dist/bundle.js'; document.head.appendChild(s) })();`}
+      </code>
+      <p>then replace all this in src/App.js and change your entry selector in index.js</p>
+      <h4>Add to Target</h4>
+      <p>Once you're done developing, do 'npm run build' and copy the contents of dist/bundle.js to paste in a script tag</p>
+    </div>
+  )
 }
 export default App
