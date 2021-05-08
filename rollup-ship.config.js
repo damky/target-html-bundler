5377605083ab77e911625c2c8b088e13da5862a4
Doug McKay <18455931+damky@users.noreply.github.com>
2021-05-08 07:56:27 -0600

    added ship config file

--- rollup-ship.config.js
+++ rollup-ship.config.js
@@ -4,11 +4,12 @@ import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
 import { terser } from 'rollup-plugin-terser'
+// import typescript from '@rollup/plugin-typescript'
 
 export default {
   input: 'src/index.js',
   output: {
-    file: 'dist/bundle.js',
+    file: 'dist/ship/bundle.js',
     format: 'iife',
     plugins: terser()
   },
@@ -26,6 +27,7 @@ export default {
       'process.env.NODE_ENV': JSON.stringify('production'),
       preventAssignment: true
     }),
+    // typescript(),
     babel({
       presets: ['@babel/preset-react', '@babel/preset-env'],
       babelHelpers: 'inline'
